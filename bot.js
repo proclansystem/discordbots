ac209fdbcafcc6bd608a5142e9a9db09714568b8
Sytrabot <42020186+Sytrabot@users.noreply.github.com>
2018-08-02 07:12:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -27,5 +26,50 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
+
+ client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+ هلا ي قلبي :heart: 
+:champagne_glass: :honey_pot: 
+كيفك ي عسل؟ ؟
+
+بالله صوت ل خويي المتسابق رقم 2 و 11 :sparkles: 
+" 
+صوتت؟؟
+
+الله يسعدك ي رب " 
+
+واذا م صوتت بالله صوت :joy: :purple_heart: 
+
+https://discord.gg/X4HkmVg       
+لا تسحب علي يا قلبي ... [ ${member}  ]
+**`)
+}).catch(console.error)
+
+})
+ 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+ هلا ي قلبي :heart: 
+:champagne_glass: :honey_pot: 
+كيفك ي عسل؟ ؟
+
+بالله صوت ل خويي المتسابق رقم 2 و 11 :sparkles: 
+" 
+صوتت؟؟
+
+الله يسعدك ي رب " 
+
+واذا م صوتت بالله صوت :joy: :purple_heart: 
+
+https://discord.gg/X4HkmVg                     
+لا تسحب علي يا قلبي ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
 
 client.login(process.env.BOT_TOKEN);
