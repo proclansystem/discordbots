7315fad3e82ba003c89d4729372fb4f736a56ef9
Sytrabot <42020186+Sytrabot@users.noreply.github.com>
2018-08-28 10:44:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,6 @@ const fetchVideoInfo = require('youtube-info');
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
 const prefix = '$';
 client.on('ready', function() {
-    client.user.setGame(`ء BoNdOK-$`,"http://twitch.tv/sytra_ayman")
     console.log(`i am ready ${client.user.username}`);
 });
 /*
@@ -258,25 +257,25 @@ client.on('message', message => {
 
 
 client.on('message', message => {
-var prefix = "$";
+var prefix = "$vip";
  
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
   if (message.author.id == '395462979115679755' ) return;
  
-if (message.content.startsWith(prefix + 'stream')) {
+if (message.content.startsWith(prefix + 'setstream')) {
   client.user.setGame(argresult, "https://www.twitch.tv/sytra_ayman");
     message.channel.sendMessage(`**${argresult}** :تم تغيير الحالة الى ستريمنج`)
 } else
  
-if (message.content.startsWith(prefix + 'name')) {
+if (message.content.startsWith(prefix + 'setname')) {
   client.user.setUsername(argresult).then
       message.channel.sendMessage(`**${argresult}** : تم تغير الأسم`)
   return message.reply("**لا تستطيع تغير الأسم الا بعد ساعتين**");
 } else
 
-if (message.content.startsWith(prefix + 'img')) {
+if (message.content.startsWith(prefix + 'setimg')) {
   client.user.setAvatar(argresult);
     message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
 }
